1fbaf205a91ba9c358b90ea948d5d2b092a38e41
Rasikh Morani <rasikhmorani@gmail.com>
2024-05-31 08:45:11 -0500

    Add Setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='omnibot',
+    version='.01',
+    packages=[''],
+    url='',
+    license='MIT',
+    author='Rasikh Morani',
+    author_email='rasikh@arcadiamgroup.com',
+    description='Python Based Script to Track Omni Token Deployments'
+)
